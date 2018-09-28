dc0fddf9bfc651c14bf64e8f75fd132af5536fbc
Igor Lameirão <igorlameirao@hotmail.com>
2018-09-28 11:45:01 -0300

    App01 - Concluido.

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Serviço/Modelo/Endereco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Serviço.Modelo
+{
+    public class Endereco
+    {
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Unidade { get; set; }
+        public string IBGE { get; set; }
+        public string gia { get; set; }
+
+
+
+
+    }
+}
